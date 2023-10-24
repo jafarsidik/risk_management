df66e58eda06a15290304d5765bc949304231460
Jafarsidik <jeff.sidik@gmail.com>
2023-10-24 23:13:33 +0700

    g

---
+++ frappe_risk_management/risk_maturity_self_assesment/doctype/risk_maturity_self_assesment/risk_maturity_self_assesment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Muhamad Jafar Sidik and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Risk Maturity Self Assesment', {
+	// refresh: function(frm) {
+
+	// }
+});
